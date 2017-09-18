6bf6ece8de044cef68c6645db77213034ebb3abf
yoojenni <31713870+yoojenni@users.noreply.github.com>
2017-09-17 22:12:42 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,68 @@
+console.log("Up and running!");
+var cardsInPlay = []
+var card = ["queen", "queen", "king", "king"];
+
+//var cardOne = "queen";
+//var cardTwo = "queen";
+//var cardThree = "king";
+//var cardFour = "king";
+console.log("User flipped " + card[0]);
+console.log("User flipped " + card[1]);
+console.log("User flipped " + card[2]);
+console.log("User flipped " + card[3]);
+
+card[0] = 'queen';
+card[1] = 'queen';
+card[2] = 'king';
+card[3] = 'king';
+
+cardsInPlay.push('cardOne');
+cardsInPlay.push('cardTwo');
+
+if (cardsInPlay[0] === cardsInPlay[1]) {
+console.log("You found a match!");
+} else {
+console.log("Sorry, try again.");
+}
+
+var card = [
+{
+	rank:'queen'
+	suite:'diamonds'
+	cardImage:"images/queen-of-diamonds.png"
+}{
+	rank: 'queen',
+	suite: "hearts",
+	cardImage: "images/queen-of-hearts.png",
+},
+
+{
+	rank: 'king'
+	suite:'diamonds'
+	cardImage: "images/king-of-diamonds.png"
+
+}
+{
+	rank: 'king'
+	suite:'hearts'
+	cardImage: "images/king-of-hearts.png"
+}
+];
+
+console.log("User flipped " + cards['queen'].rank);
+console.log("User flipped " + cards['king'].rank);
+
+this.getAttribute('queen''diamond');
+console.log(cards['queen']."images/queen-of-diamonds.png")
+
+this.getAttribute('queen''heart');
+console.log(cards['queen']."images/queen-of-hearts.png")
+
+this.getAttribute('king''diamonds');
+console.log(cards['king']."images/king-of-diamonds.png")
+
+this.getAttribute('king''hearts');
+console.log(cards['king']."images/king-of-hearts.png")
+
+
+
